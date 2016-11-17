5ffb887e64aca6dd301f38257c7d5de6e98abcbd
user <shiauhubanban>
2016-11-17 16:21:31 +0800

    v4

--- app/src/main/java/com/org/iii/shine07/MainActivity.java
+++ app/src/main/java/com/org/iii/shine07/MainActivity.java
@@ -1,25 +1,49 @@
 package com.org.iii.shine07;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
-    private Button left,right;
+    private Button left, right;
     private TextView clock;
-    boolean isRunning;
+    private boolean isRunning;
+    private int Counter;
+    private Timer timer;
+    private ClockTask clockTask;
+    private UIHandler uiHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        timer = new Timer();
+        uiHandler = new UIHandler();
+
         left = (Button)findViewById(R.id.btnleft);
         right = (Button)findViewById(R.id.btnright);
-        clock=(TextView)findViewById(R.id.timer);
+        clock = (TextView)findViewById(R.id.timer);
+
     }
+
+    @Override
+    public void finish() {
+        if (timer != null){
+            timer.purge();
+            timer.cancel();
+            timer = null;
+        }
+        super.finish();
+    }
+
     public void doLeft(View v){
 
     }
@@ -27,18 +51,19 @@ public class MainActivity extends AppCompatActivity {
         isRunning = !isRunning;
         right.setText(isRunning?"Stop":"Start");
         left.setText(isRunning?"Lap":"Reset");
-        
-        if(isRunning){
+
+        if (isRunning){
             doStart();
-        }else {
+        }else{
             doStop();
         }
     }
     private void doStart(){
-
+        clockTask = new ClockTask();
+        timer.schedule(clockTask, 10, 10);
     }
     private void doStop(){
-
+        clockTask.cancel();
     }
     private void doReset(){
 
@@ -46,4 +71,20 @@ public class MainActivity extends AppCompatActivity {
     private void doLap(){
 
     }
+
+    private class ClockTask extends TimerTask {
+        @Override
+        public void run() {
+            Counter++;
+            uiHandler.sendEmptyMessage(0);
+        }
+    }
+    private class UIHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            clock.setText("" + Counter);
+        }
+    }
+
 }
