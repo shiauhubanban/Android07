6f6212b431fb16c074c4b2c1a9b224567f949086
user <shiauhubanban>
2016-11-21 16:30:09 +0800

    v5 碼表

---
+++ app/src/main/java/com/org/iii/shine07/GameView.java
@@ -0,0 +1,12 @@
+package com.org.iii.shine07;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class GameView extends View{
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+}
